27c5e15cb3cf5fdc64021a9f7f113e6ed3e40e08
ScripterGo <anton.magnusson0417@gmail.com>
2023-03-28 12:22:43 +0200

    asd

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ use core::panic::PanicInfo;
 
 
 /// This function is called on panic.
+/* 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+*/
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
