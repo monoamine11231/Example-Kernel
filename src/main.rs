b95b4cebf73ce8447de86bd46484d39972e5f13f
ScripterGo <anton.magnusson0417@gmail.com>
2023-03-31 12:21:24 +0200

    storing changes

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,29 @@ pub extern "C" fn _start() -> ! {
     let mut writer = tooling::vga::VGAWriter::new();
     write!(writer, "Hello World!").unwrap();
     writer.newline();
-    let mut arg : u64 = 5;
+    let mut arg: u64 = 5;
     write_to_linear_buffer(&mut arg, &mut writer);
     //inline_assmebly_test(&mut writer);
-    //panicking_function(&mut writer); 
+    //panicking_function(&mut writer);
     loop {}
 }
 
-fn write_to_linear_buffer(test_arg : &mut u64, writer: &mut tooling::vga::VGAWriter){
-    let mut t : u64;
-    let mut q : u64;
-    
+fn write_to_linear_buffer(test_arg: &mut u64, writer: &mut tooling::vga::VGAWriter) {
+    let mut t: u64;
+    let mut q: u64;
+
     t = 5;
     q = 6;
-    let mut c = t+q;
+    let mut c = t + q;
     (*test_arg) = c;
     inline_assmebly_test(writer);
 }
 
-fn inline_assmebly_test(writer: &mut tooling::vga::VGAWriter){
-    let mut saved_rbp : u64;
-    let mut saved_rsp : u64;
+fn inline_assmebly_test(writer: &mut tooling::vga::VGAWriter) {
+    let mut saved_rbp: u64;
+    let mut saved_rsp: u64;
 
-    unsafe{
+    unsafe {
         asm!("
             mov {0}, rbp
             mov {1}, rsp
@@ -46,7 +46,7 @@ fn inline_assmebly_test(writer: &mut tooling::vga::VGAWriter){
     writer.newline();
 }
 
-fn panicking_function(writer: &mut tooling::vga::VGAWriter){
+fn panicking_function(writer: &mut tooling::vga::VGAWriter) {
     //write_str_at("Panicking function call", 0, 0, 0xb);
     tooling::panic_handler::stack_trace(writer);
     //panic!("This is a test panic.");
