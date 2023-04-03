29506a812c33c05cc25076077b3f2a7c4250274e
Movitz Sunar <movitz.sunar@gmail.com>
2023-04-03 12:38:13 +0200

    idt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+#![feature(strict_provenance)]
+#![feature(abi_x86_interrupt)]
+#![allow(unused, unconditional_panic)]
 
+#[macro_use]
+extern crate lazy_static;
+
+mod bord;
+mod handlers;
 mod tooling;
 
+use core::{arch::asm, panic};
+
+use bord::*;
 use tooling::vga::write_str_at;
 
 #[no_mangle]
-#[link_section = ".start"] 
+#[link_section = ".start"]
 pub extern "C" fn _start() -> ! {
-    write_str_at("Hello World!", 2, 10, 0xb);
-    //panicking_function();
+    load_idt(&IDTX);
+    write_str_at("Hello World!", 0, 0, 0xb);
+
+    let mut hej = 1;
+    unsafe {
+        asm!(
+            "div {0:e}",
+            in(reg) 0,
+            inout("eax") hej
+        )
+    }
+
     loop {}
+}
+
+lazy_static! {
+    static ref IDTX: IDT = IDT {
+        page_fault: IDTEntry::new(handlers::page_fault, Ring::Zero),
+        divide_by_zero: IDTEntry::new(handlers::zero_div, Ring::Zero),
+        ..IDT::default()
+    };
 }
 
 fn panicking_function() -> ! {
