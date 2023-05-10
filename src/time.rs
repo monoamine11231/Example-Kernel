a7a41690056c8469290b0a9e832a6c0dc9a0f8a4
salticecream <shortgolind@gmail.com>
2023-05-10 02:05:51 +0200

    fmt, fix comments

--- src/time.rs
+++ src/time.rs
@@ -1,10 +1,9 @@
 /* module for generating interrupts every millis
 (hopefully "async") so we can time stuff in a
 somewhat reliable fashion.
-hopefully it will be 
  */
 
-/* notes 
+/* notes
 
     the "natural" frequency of the timer is 1193181.18 Hz
     so if you want to create a millis interrupt,
@@ -13,14 +12,14 @@ hopefully it will be
 */
 use crate::tooling::serial::*;
 
-const DIVISOR: u16 = 1193;  // == 1193181 / 1000 hz
+const DIVISOR: u16 = 1193; // == 1193181 / 1000 hz
 
 pub static mut MILLIS: u64 = 0;
 pub static mut TIMER: Timer = Timer {
     max: 500,
     cur: 0,
     func: &say_hi,
-    active: false
+    active: false,
 };
 
 #[repr(C)]
@@ -29,7 +28,7 @@ pub struct Timer<'a> {
     max: u64,
     cur: u64,
     func: &'a dyn Fn(),
-    active: bool
+    active: bool,
 }
 
 // set the PIT to 1000 interrupts/sec, instead of the default 18
@@ -38,26 +37,25 @@ pub fn init() {
     outb(0x40, (DIVISOR & 0xff) as u8); // put low byte of DIVISOR in 0x40
     outb(0x40, ((DIVISOR >> 8) & 0xff) as u8); // put high byte
 
-    outb(0x21, inb(0x21) & 0xFE);  // chat gippity, i have no idea wtf this does
-                                                    // however very ugly panic if this line is omitted
+    outb(0x21, inb(0x21) & 0xFE); // chat gippity, i have no idea wtf this does
+                                  // however very ugly panic if this line is omitted
 }
 
 #[inline]
 pub fn get_millis() -> u64 {
-    unsafe {MILLIS}
+    unsafe { MILLIS }
 }
 
 // yeah this rust syntax makes me want to vomit
 // but basically you can pass a fn into Timer and it will be executed when the timer is done
 // only empty void functions are supported atm, hopefully somebody who knows rust can add params and return value (wrapped in Option?)
 impl<'a> Timer<'a> {
-
     pub fn new(time: u64, function: &'a dyn Fn()) -> Self {
         Timer {
             max: time,
             cur: 0,
             func: function,
-            active: false
+            active: false,
         }
     }
 
@@ -72,21 +70,24 @@ impl<'a> Timer<'a> {
         self.active = false;
     }
 
-    pub fn tick(&mut self){
+    // increment the timer by 1 ms
+    pub fn tick(&mut self) {
         if self.active {
             self.cur += 1;
             if self.cur >= self.max {
                 (self.func)();
                 // self.active = false;
+                // commented out because the timer stops ticking even after restarted
             }
         }
     }
-
 }
 
 pub fn say_hi() {
-    println!("A random number between 1 and 100: {}", unsafe {crate::misc::rand::RNG.normalized_f64()});
-    unsafe {TIMER.init();}
+    println!("A random number between 1 and 100: {}", unsafe {
+        crate::misc::rand::RNG.range(1, 100)
+    });
+    unsafe {
+        TIMER.init();
+    }
 }
-
-
